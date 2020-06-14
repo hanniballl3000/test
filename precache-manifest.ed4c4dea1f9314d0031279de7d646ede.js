59bae816ec1d0fd864aaf62595266c5f889e6a89
mohammadhosein <mohammadhosein.forbusiness@gmail.com>
2020-06-14 23:33:47 +0430

    init

---
+++ precache-manifest.ed4c4dea1f9314d0031279de7d646ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7984a14376eb556027c148804a35ca59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd2e7f67aa14eb59ca72",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4131c98c6862ce10b98b",
+    "url": "/static/js/2.a8d1fb2d.chunk.js"
+  },
+  {
+    "revision": "801455085fc6f57b23eb009849543052",
+    "url": "/static/js/2.a8d1fb2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2e7f67aa14eb59ca72",
+    "url": "/static/js/main.7eb98c22.chunk.js"
+  },
+  {
+    "revision": "58a3f1c5e48d3bac21b4",
+    "url": "/static/js/runtime-main.a0c30a24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
